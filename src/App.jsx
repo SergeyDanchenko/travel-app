05a3c32988f1f020adb6782de72de0c923acada3
IrinaKorbut <irisharomka96@gmail.com>
2021-03-05 23:15:12 +0300

    feat: add boodstrap-react and create app component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <Container>
+      Hello! Here will be the best project :)
+    </Container>
+  );
+}
+
+export default App;
