cfffd42f52811f6ceee787f2228f747ec03131fb
IrinaKorbut <irisharomka96@gmail.com>
2021-03-09 15:54:47 +0300

    feat: add player for movie

--- src/Components/CountryPage/Movie/CountryMovie.jsx
+++ src/Components/CountryPage/Movie/CountryMovie.jsx
@@ -1,10 +1,10 @@
+import Player from './Player'
 import movie from './CountryMovie.module.scss'
-
 
 function CountryMovie() {
     return (
         <div className={movie.wrapper}>
-            <div className={movie.player}>Movie player</div>
+            <Player />
         </div>
     )
 }
